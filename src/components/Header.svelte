f12bdf3f45b51dd51ff5aa447302eb7acc897cc9
Vinay Raikar <rockraikar@gmail.com>
2019-11-21 15:11:45 +0530

    Fixed Header menu styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -37,14 +37,25 @@
         class:hidden={!menuShown}
         class:block={menuShown}
         class="text-sm leading-tight align-top absolute right-0 top-0">
+        <div class="flex justify-end">
+          <span
+            on:click={toggleMenu}
+            class=" font-extrabold cursor-pointer bg-gray-100 rounded-full w-8
+            h-8 block p-1">
 
-        <span
-          on:click={toggleMenu}
-          class=" text-white font-extrabold cursor-pointer bg-gray-900
-          rounded-full w-8 h-8 block">
-          x
-        </span>
-        <div class="bg-white rounded shadow-md ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24">
+              <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41
+                19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+          </span>
+        </div>
+        <div class="bg-white rounded shadow-md mt-1">
           <div class="flex border-b p-2">
             <img
               class="inline rounded-full w-8 h-8 pointer"
