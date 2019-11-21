40a779d86fbe33757393de2bc1626eb0d58fe892
Vinay Raikar <rockraikar@gmail.com>
2019-11-21 11:07:51 +0530

    menu design

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,40 +4,71 @@
 
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
+
+  let menuShown = false;
+
+  const toggleMenu = () => {
+    console.log("toggleMenu", menuShown);
+    menuShown = !menuShown;
+  };
 </script>
 
 <header class="bg-blue-100 py-2 px-4 shadow leading-loose">
-  <div class="container mx-auto flex justify-between">
+  <div class="container mx-auto flex justify-between relative">
     <h1 class="text-xl font-bold text-blue-600 hover:text-blue-500 italic">
       <a href="/">gaDash</a>
     </h1>
     {#if gapiAuthenticated}
       <div>
         {#if gapiUser}
-          <div class="flex">
+          <div class="relative">
             <img
-              class="inline rounded-full w-8 h-8"
+              on:click={toggleMenu}
+              class="inline rounded-full w-8 h-8 cursor-pointer"
               src={gapiUser.imageUrl}
               alt="Profile image" />
-            <div
-              class=" flex-col text-sm leading-tight align-top pl-1 hidden
-              md:flex">
-              <span>{gapiUser.name}</span>
-              <span>({gapiUser.email})</span>
-              <span>
-                <button
-                  class="rounded bg-red-600 text-gray-200 p-1"
-                  on:click={() => {
-                    dispatch('logout', {});
-                  }}>
-                  Logout
-                </button>
-              </span>
-            </div>
-
           </div>
         {/if}
       </div>
     {/if}
+
+    {#if gapiAuthenticated && gapiUser}
+      <div
+        class:hidden={!menuShown}
+        class:block={menuShown}
+        class="text-sm leading-tight align-top absolute right-0 top-0">
+
+        <span
+          on:click={toggleMenu}
+          class=" text-white font-extrabold cursor-pointer bg-gray-900
+          rounded-full w-8 h-8 block">
+          x
+        </span>
+        <div class="bg-white rounded shadow-md ">
+          <div class="flex border-b p-2">
+            <img
+              class="inline rounded-full w-8 h-8 pointer"
+              src={gapiUser.imageUrl}
+              alt="Profile image" />
+            <div class=" pl-2">
+              {gapiUser.name}
+              <br />
+              ({gapiUser.email})
+            </div>
+          </div>
+          <div class="text-red-800 p-2">
+            <a
+              href="javascript:void()"
+              class=""
+              on:click={() => {
+                dispatch('logout', {});
+              }}>
+              Logout
+            </a>
+          </div>
+        </div>
+      </div>
+    {/if}
+
   </div>
 </header>
